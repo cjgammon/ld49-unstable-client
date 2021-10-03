711d4ffa4dd072114d780292b858c6626e5c7108
cjgammon <gammon@adobe.com>
2021-10-03 10:59:21 -0500

    add card images

--- src/scripts/app/game/card.tsx
+++ src/scripts/app/game/card.tsx
@@ -4,11 +4,17 @@ export default class Card{
     x: number;
     y: number;
     side: number = 0;
+    img: HTMLImageElement
+    w: number = 200;
+    h: number = 300;
 
     constructor(x?, y?, value?) {
         this.x = x || 0;
         this.y = y || 0;
         this.value = value || Math.floor(Math.random() * 100);
+
+        this.img = new Image();
+        this.img.src = `./assets/images/cards/card${Math.floor(Math.random() * 3)}.png`;
     }
 
     flip() {
@@ -22,20 +28,27 @@ export default class Card{
         ctx.strokeStyle = 'red';
         ctx.lineWidth = 2;
         ctx.beginPath();
-        ctx.rect(0, 0, 100, 100);
+        ctx.rect(0, 0, this.w, this.h);
         ctx.closePath();
         ctx.stroke();
 
         if (this.side == 1) {
             ctx.fillStyle = 'white';
-            ctx.fillRect(0, 0, 100, 100);
+            ctx.fillRect(0, 0, this.w, this.h);
+
+            if (this.img.complete) {
+                ctx.drawImage(this.img, 0, 0);
+            }
     
             ctx.fillStyle = 'black';
-            ctx.font = '48px serif';
-            ctx.fillText(`${this.value}`, 10, 50);
+            ctx.font = '20px serif';
+            let valueText = this.value.toString();
+            let valueTextPadding = 20;
+            let valueTextX = this.w - ctx.measureText(valueText).width - (valueTextPadding / 2);
+            ctx.fillText(valueText, valueTextX, valueTextPadding);
         } else {
             ctx.fillStyle = 'blue';
-            ctx.fillRect(0, 0, 100, 100);
+            ctx.fillRect(0, 0, this.w, this.h);
         }
 
         ctx.restore();
--- src/scripts/app/gameCanvas.tsx
+++ src/scripts/app/gameCanvas.tsx
@@ -21,8 +21,8 @@ export default class GameCanvas{
         let centerX = this.width / 2;
 
         for (let i = 0; i < 100; i++) {
-            this.CARDS_MINE.push(new Card(centerX + i, this.height - 100));
-            this.CARDS_THEIRS.push(new Card(centerX + i, 0));
+            this.CARDS_MINE.push(new Card(centerX + i, this.height - 150));
+            this.CARDS_THEIRS.push(new Card(centerX + i, -150));
         }
 
         window.addEventListener('click', (e) => this.handle_CLICK(e));
@@ -47,14 +47,13 @@ export default class GameCanvas{
 
         let playAnim = gsap.timeline();
 
-
         let offsetX = (this.CARDS_TABLE.length / 2) * 100;
         let posX = centerX + offsetX;
 
         //draw cards
         playAnim.to(myCard, {
             x: posX, 
-            y: centerY + 50, 
+            y: centerY, 
             duration: 0.4, 
             onComplete: () => {
                 myCard.flip();
@@ -63,35 +62,40 @@ export default class GameCanvas{
 
         playAnim.to(theirCard, {
             x: posX, 
-            y: centerY - 100, 
+            y: centerY - theirCard.h, 
             duration: 0.4, 
             onComplete: () => {
                 theirCard.flip();
             }
         }, 0);
 
-        playAnim.call(() => {
-
-            let myCardWin;
-            if (myCard.value > theirCard.value) {
-                myCardWin = true;
-            } else if (myCard.value < theirCard.value) {
-                myCardWin = false;
-            }
-
-            let cardA = this.CARDS_MINE.splice(myIndex, 1);
-            let cardB = this.CARDS_THEIRS.splice(theirIndex, 1);
-            this.CARDS_TABLE = this.CARDS_TABLE.concat(cardA, cardB);
-
-            if (myCard.value === theirCard.value) {
-                this.playCard();
-                return;
-            }
-
-            this.collectCard(myCardWin);
-
-        }, [], '+=1');
+        playAnim.call(() => this.evaluateCard(), [], '+=1.5');
       
+    }
+
+    evaluateCard() {
+        let myIndex = this.CARDS_MINE.length - 1;
+        let theirIndex = this.CARDS_THEIRS.length - 1;
+        const myCard = this.CARDS_MINE[myIndex];
+        const theirCard = this.CARDS_THEIRS[theirIndex];
+
+        let myCardWin;
+        if (myCard.value > theirCard.value) {
+            myCardWin = true;
+        } else if (myCard.value < theirCard.value) {
+            myCardWin = false;
+        }
+
+        let cardA = this.CARDS_MINE.splice(myIndex, 1);
+        let cardB = this.CARDS_THEIRS.splice(theirIndex, 1);
+        this.CARDS_TABLE = this.CARDS_TABLE.concat(cardA, cardB);
+
+        if (myCard.value === theirCard.value) {
+            this.playCard();
+            return;
+        }
+
+        this.collectCard(myCardWin);
     }
 
     collectCard(myCardWin) {
@@ -101,7 +105,7 @@ export default class GameCanvas{
             this.interactive = true;
         }});
 
-        let winnerY = myCardWin ? this.height : 0;
+        let winnerY = myCardWin ? this.height - 150 : -150;
         collectAnim.to(this.CARDS_TABLE, {
             x: centerX, 
             y: winnerY, 
